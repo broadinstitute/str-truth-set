d203a49067cbb2a03620bef834d504d0ebc2d509
bw2 <ben.weisburd@gmail.com>
2022-12-02 12:11:03 -0500

    added bash scripts for running pipelines and downstream analysis steps

---
+++ step2__upload_files_to_STR_truth_set_bucket.sh
@@ -0,0 +1,41 @@
+set -ex
+
+gsutil -m cp STR_truthset.v1.*.bed.gz* STR_truthset.v1.*.tsv* STR_truthset.v1.vcf*   gs://str-truth-set/hg38/
+
+gsutil -m rm -rf gs://str-truth-set/hg38/variant_catalogs/
+gsutil -m cp tool_comparison/variant_catalogs/positive_loci.bed.gz* tool_comparison/variant_catalogs/negative_loci.bed.gz* gs://str-truth-set/hg38/
+
+gsutil -m cp -r tool_comparison/variant_catalogs/expansion_hunter  gs://str-truth-set/hg38/variant_catalogs/
+gsutil -m cp -r tool_comparison/variant_catalogs/gangstr           gs://str-truth-set/hg38/variant_catalogs/
+gsutil -m cp -r tool_comparison/variant_catalogs/hipstr            gs://str-truth-set/hg38/variant_catalogs/
+
+set +x
+
+for i in ref/full.38.bed.gz \
+	     ref/full.38.bed.gz.tbi \
+	     ref/other/GRCh38GenomicSuperDup.without_decoys.sorted.bed.gz \
+	     ref/other/GRCh38GenomicSuperDup.without_decoys.sorted.bed.gz.tbi \
+	     ref/other/MANE.v1.0.ensembl_genomic.sorted.gtf.gz \
+	     ref/other/MANE.v1.0.ensembl_genomic.sorted.gtf.gz.tbi \
+	     ref/other/gencode.v42.annotation.sorted.gtf.gz \
+	     ref/other/gencode.v42.annotation.sorted.gtf.gz.tbi \
+	     ref/other/hg38_ver17.fixed.bed.gz \
+	     ref/other/hg38_ver17.fixed.bed.gz.tbi \
+	     ref/other/illumina_variant_catalog.sorted.bed.gz \
+	     ref/other/illumina_variant_catalog.sorted.bed.gz.tbi \
+	     ref/other/known_disease_associated_STR_loci.GRCh38.bed.gz \
+	     ref/other/known_disease_associated_STR_loci.GRCh38.bed.gz.tbi \
+	     ref/other/repeat_specs_GRCh38_allowing_mismatches.sorted.trimmed.at_least_6bp.bed.gz \
+	     ref/other/repeat_specs_GRCh38_allowing_mismatches.sorted.trimmed.at_least_6bp.bed.gz.tbi \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_6bp.bed.gz \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_6bp.bed.gz.tbi \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_9bp.bed.gz \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_9bp.bed.gz.tbi \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_12bp.bed.gz \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_12bp.bed.gz.tbi \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_15bp.bed.gz \
+	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_15bp.bed.gz.tbi;
+do
+    echo ${i}
+    gsutil -m cp ${i} gs://str-truth-set/hg38/${i}
+done
---
+++ step3__run_tool_pipelines.sh
@@ -0,0 +1,49 @@
+# Run tool pipelines using Hail Batch
+
+set -ex
+
+function run_pipelines {
+    input_bam=$1
+    output_dir=$2
+    local_dir=$3
+
+    # ExpansionHunterDenovo
+    python3 ./tool_comparison/hail_batch_pipelines/expansion_hunter_denovo_pipeline.py --input-bam ${input_bam} --output-dir ${output_dir}/expansion_hunter_denovo
+    
+    mkdir -p ${local_dir}/expansion_hunter_denovo
+    gsutil -m cp ${output_dir}/expansion_hunter_denovo/CHM1_CHM13_2.locus.tsv ${local_dir}/expansion_hunter_denovo/
+    
+    # ExpansionHunter
+    python3 ./tool_comparison/hail_batch_pipelines/expansion_hunter_pipeline.py  --positive-loci --input-bam ${input_bam} --output-dir ${output_dir}/expansion_hunter
+    python3 ./tool_comparison/hail_batch_pipelines/expansion_hunter_pipeline.py  --negative-loci --input-bam ${input_bam} --output-dir ${output_dir}/expansion_hunter
+    
+    mkdir -p ${local_dir}/expansion_hunter/positive_loci/      ${local_dir}/expansion_hunter/negative_loci/
+    gsutil -m cp ${output_dir}/expansion_hunter/positive_loci/combined.positive_loci.*_json_files.*.tsv.gz ${local_dir}/expansion_hunter/positive_loci/
+    gsutil -m cp ${output_dir}/expansion_hunter/negative_loci/combined.negative_loci.*_json_files.*.tsv.gz ${local_dir}/expansion_hunter/negative_loci/
+    
+    # GangSTR
+    python3 ./tool_comparison/hail_batch_pipelines/gangstr_pipeline.py  --positive-loci --input-bam ${input_bam} --output-dir ${output_dir}/gangstr
+    python3 ./tool_comparison/hail_batch_pipelines/gangstr_pipeline.py  --negative-loci --input-bam ${input_bam} --output-dir ${output_dir}/gangstr
+    
+    mkdir -p ${local_dir}/gangstr/positive_loci/      ${local_dir}/gangstr/negative_loci/
+    gsutil -m cp ${output_dir}/gangstr/positive_loci/combined.positive_loci.*_json_files.*.tsv.gz ${local_dir}/gangstr/positive_loci/
+    gsutil -m cp ${output_dir}/gangstr/negative_loci/combined.negative_loci.*_json_files.*.tsv.gz ${local_dir}/gangstr/negative_loci/
+}
+
+# Run pipelines on original bam
+run_pipelines \
+  "gs://str-truth-set/hg38/CHM1_CHM13_2.bam" \
+  "gs://str-truth-set/hg38/tool_results" \
+  "./tool_comparison/results"
+
+# Downsample to 30x
+python3 ./tool_comparison/hail_batch_pipelines/downsample_bam_pipeline.py --verbose --target-coverage 30
+
+# Rerun pipelines on downsampled bam
+run_pipelines \
+  "gs://str-truth-set/hg38/CHM1_CHM13_2.downsampled_to_30x.bam" \
+  "gs://str-truth-set/hg38/tool_results_for_downsampled_30x_bam" \
+  "./tool_comparison/results_for_downsampled_30x_bam"
+
+
+
