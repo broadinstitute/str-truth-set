4735f5d7cbd8b5d67397f64f7d95580e4a2c8d4c
bw2 <ben.weisburd@gmail.com>
2022-12-22 16:18:51 -0500

    moved compare_tool_results.sh to step4__combine_tool_results.sh

---
+++ step4__combine_tool_results.sh
@@ -0,0 +1,58 @@
+set -x
+set -e
+set -u
+
+for results_folder in results_for_downsampled_10x_bam ; #results_for_exome  results  results_for_downsampled_30x_bam  results_for_downsampled_20x_bam  results_for_downsampled_10x_bam  results_for_downsampled_5x_bam
+do
+  echo Processing $results_folder ...
+  python3 tool_comparison/scripts/compute_truth_set_tsv_for_comparisons.py \
+    --output-dir ./tool_comparison/${results_folder}/  \
+    STR_truthset.v1.variants.tsv.gz \
+    ./tool_comparison/variant_catalogs/negative_loci.tsv.gz
+
+  # ExpansionHunter
+  python3 tool_comparison/scripts/add_tool_results_columns.py \
+      --tool ExpansionHunter \
+     ./tool_comparison/${results_folder}/expansion_hunter/positive_loci/combined.positive_loci.*_json_files.variants.tsv.gz \
+     ./tool_comparison/${results_folder}/STR_truthset.v1.variants.for_comparison.tsv.gz
+
+  python3 tool_comparison/scripts/add_tool_results_columns.py \
+      --tool ExpansionHunter \
+      ./tool_comparison/${results_folder}/expansion_hunter/negative_loci/combined.negative_loci.*_json_files.variants.tsv.gz \
+      ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+
+  mv ./tool_comparison/${results_folder}/STR_truthset.v1.variants.for_comparison.with_ExpansionHunter_results.tsv.gz ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+  mv ./tool_comparison/${results_folder}/negative_loci.for_comparison.with_ExpansionHunter_results.tsv.gz ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+
+  # GangSTR
+  python3 tool_comparison/scripts/add_tool_results_columns.py \
+    --tool GangSTR \
+    ./tool_comparison/${results_folder}/gangstr/positive_loci/combined.positive_loci.*_json_files.variants.tsv.gz \
+    ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+
+  python3 tool_comparison/scripts/add_tool_results_columns.py \
+    --tool GangSTR \
+    ./tool_comparison/${results_folder}/gangstr/negative_loci/combined.negative_loci.*_json_files.variants.tsv.gz \
+    ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+
+  mv ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.with_GangSTR_results.tsv.gz ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+  mv ./tool_comparison/${results_folder}/negative_loci.for_comparison.with_GangSTR_results.tsv.gz ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+
+  # concordance
+  python3 tool_comparison/scripts/add_concordance_columns.py \
+    ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+
+  python3 tool_comparison/scripts/add_concordance_columns.py \
+    ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+
+  mv ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.with_concordance.tsv.gz ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+  mv ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.with_concordance.alleles.tsv.gz ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.alleles.tsv.gz
+  mv ./tool_comparison/${results_folder}/negative_loci.for_comparison.with_concordance.tsv.gz ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+  mv ./tool_comparison/${results_folder}/negative_loci.for_comparison.with_concordance.alleles.tsv.gz ./tool_comparison/${results_folder}/negative_loci.for_comparison.alleles.tsv.gz
+
+  gunzip -f ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.tsv.gz
+  gunzip -f ./tool_comparison/${results_folder}/negative_loci.for_comparison.tsv.gz
+  gunzip -f ./tool_comparison/${results_folder}/STR_truthset.v1.for_comparison.alleles.tsv.gz
+  gunzip -f ./tool_comparison/${results_folder}/negative_loci.for_comparison.alleles.tsv.gz
+
+done
