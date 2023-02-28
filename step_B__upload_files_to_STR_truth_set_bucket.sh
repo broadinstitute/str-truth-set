77973f854c758391850dbed4dfaa8b1396d8b59e
bw2 <ben.weisburd@gmail.com>
2023-02-28 11:47:31 -0500

    updated step B

--- step_B__upload_files_to_STR_truth_set_bucket.sh
+++ step_B__upload_files_to_STR_truth_set_bucket.sh
@@ -1,14 +1,12 @@
 set -ex
 
-gsutil -m cp STR_truthset.v1.*.bed.gz* STR_truthset.v1.*.tsv* STR_truthset.v1.vcf*   gs://str-truth-set/hg38/
+gsutil -q -m cp STR_truthset.v1.*.bed.gz* STR_truthset.v1.*.tsv* STR_truthset.v1.vcf*   gs://str-truth-set/hg38/
 
-gsutil -m cp tool_comparison/variant_catalogs/positive_loci.bed.gz* tool_comparison/variant_catalogs/negative_loci.bed.gz* gs://str-truth-set/hg38/
+gsutil -q -m cp tool_comparison/variant_catalogs/positive_loci.bed.gz* tool_comparison/variant_catalogs/negative_loci.bed.gz* gs://str-truth-set/hg38/
 
-gsutil -m cp -r tool_comparison/variant_catalogs/expansion_hunter  gs://str-truth-set/hg38/variant_catalogs/
-gsutil -m cp -r tool_comparison/variant_catalogs/gangstr           gs://str-truth-set/hg38/variant_catalogs/
-gsutil -m cp -r tool_comparison/variant_catalogs/hipstr            gs://str-truth-set/hg38/variant_catalogs/
-
-exit 0
+gsutil -q -m cp -r tool_comparison/variant_catalogs/expansion_hunter  gs://str-truth-set/hg38/variant_catalogs/
+gsutil -q -m cp -r tool_comparison/variant_catalogs/gangstr           gs://str-truth-set/hg38/variant_catalogs/
+gsutil -q -m cp -r tool_comparison/variant_catalogs/hipstr            gs://str-truth-set/hg38/variant_catalogs/
 
 set +x
 
@@ -16,8 +14,10 @@ for i in ref/full.38.bed.gz \
 	     ref/full.38.bed.gz.tbi \
 	     ref/other/GRCh38GenomicSuperDup.without_decoys.sorted.bed.gz \
 	     ref/other/GRCh38GenomicSuperDup.without_decoys.sorted.bed.gz.tbi \
+	     ref/other/MANE.GRCh38.v1.0.ensembl_genomic.gtf.gz \
 	     ref/other/MANE.v1.0.ensembl_genomic.sorted.gtf.gz \
 	     ref/other/MANE.v1.0.ensembl_genomic.sorted.gtf.gz.tbi \
+	     ref/other/gencode.v42.annotation.gtf.gz \
 	     ref/other/gencode.v42.annotation.sorted.gtf.gz \
 	     ref/other/gencode.v42.annotation.sorted.gtf.gz.tbi \
 	     ref/other/hg38_ver17.fixed.bed.gz \
@@ -38,5 +38,5 @@ for i in ref/full.38.bed.gz \
 	     ref/other/repeat_specs_GRCh38_without_mismatches.sorted.trimmed.at_least_15bp.bed.gz.tbi;
 do
     echo ${i}
-    gsutil -m cp ${i} gs://str-truth-set/hg38/${i}
+    gsutil -q -m cp ${i} gs://str-truth-set/hg38/${i}
 done
