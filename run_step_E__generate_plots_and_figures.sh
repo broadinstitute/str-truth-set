9ea62e3823ced2e0f6a3542bc5e09f98b84603e7
bw2 <ben.weisburd@gmail.com>
2023-03-05 03:35:33 -0500

    .

--- run_step_E__generate_plots_and_figures.sh
+++ run_step_E__generate_plots_and_figures.sh
@@ -12,7 +12,7 @@ python3 -u plot_tool_runtime_and_memory.py
 python3 -u plot_expansion_hunter_denovo_results.py
 python3 -u plot_gene_constraint_info.py
 
-#python3 -u plot_tool_comparisons.py --verbose
+python3 -u plot_tool_comparisons.py --verbose
 
 # upload to gs://str-truth-set/
 gsutil -m cp -n *.png *.svg   gs://str-truth-set/hg38/figures/
