ec2ab5860a5eae8cb42809a2562ba8290eb2f50d
bw2 <ben.weisburd@gmail.com>
2024-05-03 07:30:26 -0400

    finished implementing tool_comparison/hail_batch_pipelines/check_bam_depth.py

--- tool_comparison/hail_batch_pipelines/check_bam_depth.py
+++ tool_comparison/hail_batch_pipelines/check_bam_depth.py
@@ -8,7 +8,7 @@ REFERENCE_FASTA_FAI_PATH = "gs://gcp-public-data--broad-references/hg38/v0/Homo_
 CHM1_CHM13_CRAM_PATH = "gs://broad-public-datasets/CHM1_CHM13_WGS2/CHM1_CHM13_WGS2.cram"
 CHM1_CHM13_CRAI_PATH = "gs://broad-public-datasets/CHM1_CHM13_WGS2/CHM1_CHM13_WGS2.cram.crai"
 
-DOCKER_IMAGE = "weisburd/gatk@sha256:b9b39f21b51ec9ef17937e6581dc3624a0a232d464616eddffb7678171fba578"
+DOCKER_IMAGE = "weisburd/filter-vcfs@sha256:9ea4e1e648e15bb370efbe59f096d54791c9b6dcb75f1ec3e957a3c897014bac"
 
 
 def main():
@@ -16,34 +16,31 @@ def main():
     parser = bp.get_config_arg_parser()
     parser.add_argument("--reference-fasta", default=REFERENCE_FASTA_PATH)
     parser.add_argument("--reference-fasta-fai")
-    parser.add_argument("--input-bam", default=CHM1_CHM13_CRAM_PATH)
-    parser.add_argument("--input-bai")
     parser.add_argument("--output-dir", required=True, help="Google storage directory for output file")
+    parser.add_argument("input_bam", nargs="+")
     args = bp.parse_known_args()
 
-    s1 = bp.new_step(pipeline_name, image=DOCKER_IMAGE, cpu=1, memory="standard", storage="20Gi", output_dir=args.output_dir)
-    local_fasta = s1.input(args.reference_fasta, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
-    if args.reference_fasta_fai:
-        s1.input(args.reference_fasta_fai, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
 
-    local_bam = s1.input(args.input_bam, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
-    if args.input_bai:
-        s1.input(args.input_bai, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
+    for input_bam in args.input_bam:
+        bam_or_cram_prefix = re.sub("(.bam|.cram)$", "", os.path.basename(input_bam))
+        s1 = bp.new_step(f"Coverage: {bam_or_cram_prefix}", image=DOCKER_IMAGE, cpu=1, memory="standard", storage="20Gi", output_dir=args.output_dir)
+        local_fasta = s1.input(args.reference_fasta, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
+        if args.reference_fasta_fai:
+            s1.input(args.reference_fasta_fai, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
 
-    bam_or_cram_prefix = re.sub("(.bam|.cram)$", "", local_bam.filename)
+        local_bam = s1.input(input_bam, localize_by=Localize.HAIL_BATCH_CLOUDFUSE)
 
+        s1.command("set -ex")
+        s1.command("wget https://github.com/brentp/mosdepth/releases/download/v0.3.5/mosdepth -O /usr/local/bin/mosdepth")
+        s1.command("chmod 777 /usr/local/bin/mosdepth")
 
-    s1.command("set -ex")
-    s1.command("wget https://github.com/brentp/mosdepth/releases/download/v0.3.5/mosdepth -O /usr/local/bin/mosdepth")
-    s1.command("chmod 777 /usr/local/bin/mosdepth")
+        s1.command(f"mosdepth -f {local_fasta} -x {bam_or_cram_prefix}.coverage {local_bam}")
+        #s1.output(f"{bam_or_cram_prefix}.coverage.mosdepth.summary.txt")
 
-    s1.command(f"mosdepth -f {local_fasta} -x {bam_or_cram_prefix}.coverage {local_bam}")
+        s1.command(f"cat {bam_or_cram_prefix}.coverage.mosdepth.summary.txt | cut -f 4 | tail -n +2 | head -n 23")
+        s1.command(f"grep total {bam_or_cram_prefix}.coverage.mosdepth.summary.txt > {bam_or_cram_prefix}.total_coverage.txt")
 
-    s1.command(f"cat {bam_or_cram_prefix}.coverage.mosdepth.summary.txt | cut -f 4 | tail -n +2 | head -n 23")
-    s1.command(f"grep total {bam_or_cram_prefix}.coverage.mosdepth.summary.txt > {bam_or_cram_prefix}.total_coverage.txt")
-
-    s1.output(f"{bam_or_cram_prefix}.coverage.mosdepth.summary.txt")
-    s1.output(f"{bam_or_cram_prefix}.total_coverage.txt")
+        s1.output(f"{bam_or_cram_prefix}.total_coverage.txt")
 
     bp.run()
 
